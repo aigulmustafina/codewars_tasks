548f3f72d2fc901bc9215762180b08dc00108ff8
Aigul Mustafina <aimustafina2016@gmail.com>
2022-05-24 23:25:36 +0600

    Adding solution and test files

---
+++ tests/test_alan_partridge_apple_turnover.py
@@ -0,0 +1,6 @@
+import pytest
+from solutions.alan_partridge_apple_turnover import apple
+def tests():
+
+    assert apple('50') == "It's hotter than the sun!!"
+    assert apple(4) == 'Help yourself to a honeycomb Yorkie for the glovebox.'
